b33a1ff8c0eb445df783d554bd5855163a3afee7
scharlez <stevencharlez@gmail.com>
2022-08-04 16:09:00 -0700

    Completed API Endpoints

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -6,23 +6,23 @@ class CatsController < ApplicationController
 
     def create 
         cat = Cat.create(cat_params)
-        render json: cats
+        render json: cat
     end 
 
     def update
         cat = Cat.find(params[:id])
         cat.update(cat_params)
-        render json: cats
+        render json: cat
     end
 
     def destroy
         cat = Cat.find(params[:id])
         cat.destroy
-        render json: cats 
+        render json: cat
     end 
 
     private
     def cat_params
-        params.requires(:cat).permit(:name, :age, :status, :looking_for, :about_me, :hobbies)
+        params.require(:cat).permit(:name, :age, :status, :looking_for, :about_me, :hobbies, :image)
     end
 end
